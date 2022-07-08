023b14bde0e1a67c61c6cbb18c12611098dcec64
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2022-07-08 17:36:54 +0100

    Initial commit of modules repo

---
+++ services/webserver-cluster/outputs.tf
@@ -0,0 +1,14 @@
+output "alb_dns_name" {
+  value       = aws_lb.web_alb.dns_name
+  description = "The domain name of the load balancer"
+}
+
+output "asg_name" {
+  value       = aws_autoscaling_group.web_asg.name
+  description = "The name of the Auto Scaling Group"
+}
+
+output "alb_security_group_id" {
+  value       = aws_security_group.web_alb.id
+  description = "The ID of the Security Group attached to the load balancer"
+}
